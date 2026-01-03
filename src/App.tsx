163a08ad179b9b78523190631645d42d88dda75a
Divyasree00 <divyasree.madduri0320@gmail.com>
2026-01-03 19:29:46 +0530

    checking

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,34 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { HashRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Chatbot from "./components/Chatbot";
 import Home from "./pages/Home";
+import StudyPlan from "./pages/StudyPlan";
+import Feedback from "./pages/Feedback";
+import NotFound from "./pages/NotFound";
 
-const App = () => {
-  return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </HashRouter>
-  );
-};
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <HashRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/study-plan" element={<StudyPlan />} />
+          <Route path="/feedback" element={<Feedback />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Chatbot />
+      </HashRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
 
 export default App;
